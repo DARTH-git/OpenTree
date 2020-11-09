4b3fe93e18371c8e69c5e90deaebb0ac04d21d33
Alan <alanyny.yang@mail.utoronto.ca>
2020-11-09 12:07:11 -0500

    updated

--- R/app.R
+++ R/app.R
@@ -12,7 +12,7 @@ library(shiny)
 
 # Functions for Save DataTable ---------------------------------------------------------------
 
-fileName <- treeName #sprintf("%s_%s.csv", as.integer(Sys.time()), digest::digest(data))
+fileName <- 'treeName' #sprintf("%s_%s.csv", as.integer(Sys.time()), digest::digest(data))
 
 # readSettings <- function(){
 #     if (file.exists(setfname)){
@@ -26,12 +26,12 @@ ui <- htmlTemplate("www/OpenTree.html",
         #text_input = textInput("fname","File name: ",value="OpenTree.csv"),
         #verb_output = verbatimTextOutput("text"),
         #btn1_checker = actionButton("chck1_file", "Check for file"),
-        #Data Table 
+        #Data Table
         #data_table = tableOutput("table1"),
-        
+
         # text output
         text_output = tableOutput("table2")
-        
+
 
 )
 
@@ -44,31 +44,31 @@ server = function(input, output, session){
     # This block fires each time we receive a message from JavaScript
     output$table2 <- renderTable({
         #Write csv
-        
+
         #print("JAVA SCRIPT LLAMADA")
         #paste("Aquí estoy", input$jsValue)
         #print(value)
         #write.csv(value,setfname)
         # value = input$jsValue
         # write.csv(value, paste0("reactiveObjects/OpenTree_", fileName,".csv"))
-        # 
+        #
        # write.csv(value, paste0("reactiveObjects/OpenTree_", format(Sys.time(), "%d-%b-%Y %H.%M"), ".csv"))
-        
-        
+
+
         #Write json file
-        
+
         #print("JSON LLAMADA")
         json_value = input$jsonData
         #write(json_value, paste0("reactiveObjects/OpenTree_",fileName, ".json"))
         write(json_value, paste0("OpenTree_",fileName, ".json"))
-        
+
     })
-    
+
 
 }
 
 
-# Run the application 
+# Run the application
 shinyApp(ui , server)
 
 
